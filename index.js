21497dd28f987fe3a4bc6bbda9143033def9eee0
Akibjohn <akibmohd285@gmail.com>
2022-10-31 20:46:59 +0530

    update

---
+++ index.js
@@ -0,0 +1,124 @@
+// var a = 10
+// var b = 2
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a%b)
+// console.log(a/b)
+// console.log(a ** b) // (**) represents power */
+
+
+// // search for ^ operator
+// console.log(a^b)
+// console.log(20 ^ 2)
+// console.log(b^a)
+// console.log(2^20)
+// // search for ^ operator
+
+
+
+// // number DATATYPE
+// const number = 3
+// console.log(typeof(number))
+
+// const number2 = 3.4
+// console.log(typeof(number2))
+// console.log(typeof(typeof(number2)))
+
+// const string = "hey"
+// console.log(typeof(string));
+// console.log(typeof(typeof(string)));
+
+// var shocked
+// console.log(shocked)
+// console.log(typeof(shocked))
+
+// const nulla = null
+// console.log(typeof(nulla));
+
+// let nulla2 = null
+// console.log(typeof(nulla2)); //object
+// console.log(typeof(typeof nulla2))
+
+// // number DATATYPE
+
+// // operators
+
+// // assignment operators
+// // use to assign values
+// // =, +=, -+, /=, *=, %=, **=
+
+// var a = 4
+// console.log("= -------->", a);
+// a += 5
+// // a += 5 = a = a+5
+// console.log("+= =====>", a);
+// a -= 5
+// console.log("-= =====>", a);
+// a /= 5
+// console.log("/= =====>", a);
+// a %= 5
+// console.log("%= =====>", a);
+// a *= 5
+// console.log("*= =====>", a);
+// a **= 5
+// console.log("**= =====>", a);
+// // assignment operators
+
+
+// // arthemetic operators
+// // +, -, /, %, ++, --, **
+
+// var b = 20
+// var c = 2
+// var d = 3
+// var e = 5
+// console.log(" + ------->",b+c);
+// console.log(" - ------->",b-c);
+// console.log(" / ------->",b/c);
+// console.log(" % ------->",b%c);
+// console.log(" * ------->",b*c);
+// console.log(" ** ------->",b**c);
+// console.log(" ++ ------->", ++b);
+// console.log(" ----------> ++", c++);
+// console.log(" -- ------->", --d);
+// console.log(" ----------> --", e--);
+
+// // arthemetic operators
+
+// // comparison operator
+// // ==, !=, ===, >, >=, <, <=
+
+// var x = 10
+// var y = 10
+// var z = "10"
+// var w = 1
+
+// console.log(x==y);
+// console.log(x==z);
+// console.log(x!=y);
+// console.log(x===y);
+// console.log(x===z);
+// console.log(x>w);
+// console.log(x<w);
+// console.log(x>=y);
+// console.log(x>=y);
+
+// // comparison operator
+
+// // logical operators
+
+// // &&, ||, !
+// console.log((x==y)&&(x==z));
+// console.log((x===y)||(x===z));
+// console.log(!(x==y));
+
+// // logical operators
+
+a=10
+b=20
+if (a > b) {
+    console.log("a is greater");
+} else {
+    console.log("b is greater");
+}
