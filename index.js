2c3af0e0ca218d55e93c26b6c4bb1a123e6cf96a
Akibjohn <akibmohd285@gmail.com>
2022-10-31 21:52:20 +0530

    update

--- index.js
+++ index.js
@@ -1,124 +1,185 @@
-// var a = 10
-// var b = 2
-// console.log(a+b)
-// console.log(a-b)
-// console.log(a*b)
-// console.log(a%b)
-// console.log(a/b)
-// console.log(a ** b) // (**) represents power */
+// // var a = 10
+// // var b = 2
+// // console.log(a+b)
+// // console.log(a-b)
+// // console.log(a*b)
+// // console.log(a%b)
+// // console.log(a/b)
+// // console.log(a ** b) // (**) represents power */
 
 
-// // search for ^ operator
-// console.log(a^b)
-// console.log(20 ^ 2)
-// console.log(b^a)
-// console.log(2^20)
-// // search for ^ operator
+// // // search for ^ operator
+// // console.log(a^b)
+// // console.log(20 ^ 2)
+// // console.log(b^a)
+// // console.log(2^20)
+// // // search for ^ operator
 
 
 
-// // number DATATYPE
-// const number = 3
-// console.log(typeof(number))
+// // // number DATATYPE
+// // const number = 3
+// // console.log(typeof(number))
 
-// const number2 = 3.4
-// console.log(typeof(number2))
-// console.log(typeof(typeof(number2)))
+// // const number2 = 3.4
+// // console.log(typeof(number2))
+// // console.log(typeof(typeof(number2)))
 
-// const string = "hey"
-// console.log(typeof(string));
-// console.log(typeof(typeof(string)));
+// // const string = "hey"
+// // console.log(typeof(string));
+// // console.log(typeof(typeof(string)));
 
-// var shocked
-// console.log(shocked)
-// console.log(typeof(shocked))
+// // var shocked
+// // console.log(shocked)
+// // console.log(typeof(shocked))
 
-// const nulla = null
-// console.log(typeof(nulla));
+// // const nulla = null
+// // console.log(typeof(nulla));
 
-// let nulla2 = null
-// console.log(typeof(nulla2)); //object
-// console.log(typeof(typeof nulla2))
+// // let nulla2 = null
+// // console.log(typeof(nulla2)); //object
+// // console.log(typeof(typeof nulla2))
 
-// // number DATATYPE
+// // // number DATATYPE
 
-// // operators
+// // // operators
 
-// // assignment operators
-// // use to assign values
-// // =, +=, -+, /=, *=, %=, **=
+// // // assignment operators
+// // // use to assign values
+// // // =, +=, -+, /=, *=, %=, **=
 
-// var a = 4
-// console.log("= -------->", a);
-// a += 5
-// // a += 5 = a = a+5
-// console.log("+= =====>", a);
-// a -= 5
-// console.log("-= =====>", a);
-// a /= 5
-// console.log("/= =====>", a);
-// a %= 5
-// console.log("%= =====>", a);
-// a *= 5
-// console.log("*= =====>", a);
-// a **= 5
-// console.log("**= =====>", a);
-// // assignment operators
+// // var a = 4
+// // console.log("= -------->", a);
+// // a += 5
+// // // a += 5 = a = a+5
+// // console.log("+= =====>", a);
+// // a -= 5
+// // console.log("-= =====>", a);
+// // a /= 5
+// // console.log("/= =====>", a);
+// // a %= 5
+// // console.log("%= =====>", a);
+// // a *= 5
+// // console.log("*= =====>", a);
+// // a **= 5
+// // console.log("**= =====>", a);
+// // // assignment operators
 
 
-// // arthemetic operators
-// // +, -, /, %, ++, --, **
+// // // arthemetic operators
+// // // +, -, /, %, ++, --, **
 
-// var b = 20
-// var c = 2
-// var d = 3
-// var e = 5
-// console.log(" + ------->",b+c);
-// console.log(" - ------->",b-c);
-// console.log(" / ------->",b/c);
-// console.log(" % ------->",b%c);
-// console.log(" * ------->",b*c);
-// console.log(" ** ------->",b**c);
-// console.log(" ++ ------->", ++b);
-// console.log(" ----------> ++", c++);
-// console.log(" -- ------->", --d);
-// console.log(" ----------> --", e--);
+// // var b = 20
+// // var c = 2
+// // var d = 3
+// // var e = 5
+// // console.log(" + ------->",b+c);
+// // console.log(" - ------->",b-c);
+// // console.log(" / ------->",b/c);
+// // console.log(" % ------->",b%c);
+// // console.log(" * ------->",b*c);
+// // console.log(" ** ------->",b**c);
+// // console.log(" ++ ------->", ++b);
+// // console.log(" ----------> ++", c++);
+// // console.log(" -- ------->", --d);
+// // console.log(" ----------> --", e--);
 
-// // arthemetic operators
+// // // arthemetic operators
 
-// // comparison operator
-// // ==, !=, ===, >, >=, <, <=
+// // // comparison operator
+// // // ==, !=, ===, >, >=, <, <=
 
-// var x = 10
-// var y = 10
-// var z = "10"
-// var w = 1
+// // var x = 10
+// // var y = 10
+// // var z = "10"
+// // var w = 1
 
-// console.log(x==y);
-// console.log(x==z);
-// console.log(x!=y);
-// console.log(x===y);
-// console.log(x===z);
-// console.log(x>w);
-// console.log(x<w);
-// console.log(x>=y);
-// console.log(x>=y);
+// // console.log(x==y);
+// // console.log(x==z);
+// // console.log(x!=y);
+// // console.log(x===y);
+// // console.log(x===z);
+// // console.log(x>w);
+// // console.log(x<w);
+// // console.log(x>=y);
+// // console.log(x>=y);
 
-// // comparison operator
+// // // comparison operator
 
-// // logical operators
+// // // logical operators
 
-// // &&, ||, !
-// console.log((x==y)&&(x==z));
-// console.log((x===y)||(x===z));
-// console.log(!(x==y));
+// // // &&, ||, !
+// // console.log((x==y)&&(x==z));
+// // console.log((x===y)||(x===z));
+// // console.log(!(x==y));
 
-// // logical operators
+// // // logical operators
 
-a=10
-b=20
-if (a > b) {
-    console.log("a is greater");
-} else {
-    console.log("b is greater");
-}
+// a=10
+// b=20
+// if (a > b) {
+//     console.log("a is greater");
+// } else {
+//     console.log("b is greater");
+// }
+
+// var maths = 0 
+// var science = 0 
+// var hindi = 0 
+// var eng = 0 
+// var sst = 0 
+// var sanskrit = 285 
+// var total =( maths + science + hindi + eng +sst + sanskrit)/6
+// console.log(total);
+
+// // var total2 = maths + science + hindi + eng +sst + sanskrit
+// // var total = `${maths}` + `${science}` + `${hindi}` + `${eng}` +`${sst}` + `${sanskrit}`
+// // var total3 = maths + "" + science
+// // var total4 = `${maths}`
+// // var total5 = maths, science, hindi
+// // var total6 = `${maths}`+ `${science}`+ `${hindi}`
+
+// // console.log("total", total);
+// // console.log("total", total2);
+// // console.log("total", total3);
+// // console.log("total", typeof(total4));
+// // console.log("total5", total5);
+
+// // if(total>90){
+// //     console.log("hey you got a+ grade");
+// // }else if(total>80 && total<=90){
+// //     console.log("hey you got a grade");
+// // }else if(total>70 && total<=80){
+// //     console.log("hey you got b+ grade");
+// // }else if(total>60 && total<=70){
+// //     console.log("hey you got b grade");
+// // }else if(total>50 && total<=60){
+// //     console.log("hey you got c+ grade");
+// // }else if(total>40 && total<=50){
+// //     console.log("hey you got c grade");
+// // }else if(total>30 && total<=40){
+// //     console.log("hey you got d grade");
+// // }else{
+// //     console.log("re lag gyi teri ");
+// // }
+
+// if(total>90){
+//     console.log("hey you got a+ grade");
+// }if(total>80 && total<=90){
+//     console.log("hey you got a grade");
+// }if(total>70 && total<=80){
+//     console.log("hey you got b+ grade");
+// }if(total>60 && total<=70){
+//     console.log("hey you got b grade");
+// }if(total>50 && total<=60){
+//     console.log("hey you got c+ grade");
+// }if(total>40 && total<=50){
+//     console.log("hey you got c grade");
+// }if(total>30 && total<=40){
+//     console.log("hey you got d grade");
+// }else{
+//     console.log("re lag gyi teri ");
+// }
+
+// suhanaMausam = false
+// suhanaMausam ? console.log("mausam suhana hai") : console.log("chal jhoote")
